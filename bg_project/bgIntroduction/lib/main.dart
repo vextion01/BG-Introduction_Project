34ef2ab456a045a374f73738d8d22165cfd6a8d3
vextion01 <91772633+vextion01@users.noreply.github.com>
2023-09-20 22:32:41 +0700

    Update main.dart
    Fix errors

--- bg_project/bgIntroduction/lib/main.dart
+++ bg_project/bgIntroduction/lib/main.dart
@@ -6,17 +6,7 @@ import 'package:flutter/material.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-      // options: const FirebaseOptions(
-      //     apiKey: "AIzaSyBgNZ5HwqP8QIIYE4vylJaBi60t-oR1O94",
-      //     authDomain: "bg-introduction-3aade.firebaseapp.com",
-      //     databaseURL:
-      //         "https://bg-introduction-3aade-default-rtdb.asia-southeast1.firebasedatabase.app",
-      //     projectId: "bg-introduction-3aade",
-      //     storageBucket: "bg-introduction-3aade.appspot.com",
-      //     messagingSenderId: "593533535477",
-      //     appId: "1:593533535477:web:c07a64b0a410954d84b1bc",
-      //     measurementId: "G-70KQKL9Q8J"),
-      );
+   
   runApp(MyApp());
 }
 
